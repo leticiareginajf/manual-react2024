4cde0822535beb5b54d8e96927f3f8780e90e388
leticiareginajf <leticia.reginajf@ggmail.com>
2024-02-22 09:16:55 -0300

    Componentes

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,12 +2,30 @@ import React from 'react'
 
 export default function App() {
 
+const canal = 'React - 2024'
+const curso = '2024'
+const name = 'Leticia Honório'
+
+
+const testando=()=>{
+
+return ('Escrevendo a mensagem...')
+
+}
+
+function mensagem(){
+
+  return ('Estamos orgulhosos de você se esforça pelo React-2024')
+
+}
   return (
   
   <section>
-  <h1>Hello World</h1> 
-  <br/>
-  <h1>Leticia Desenvolvedora</h1>
+  <h1>Curso: {canal}</h1> 
+  <h1>{curso}</h1>
+  <p>{'Desenvolvedora: ' + name}</p>
+  <>{testando()}</>
+  <><br/>{mensagem()}</>
   </section>
   )
 /*return(
