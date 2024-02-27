bbe4884493fea8df74c117c985da177cf41d10cd
leticiareginajf <leticia.reginajf@ggmail.com>
2024-02-27 10:50:13 -0300

    Componente Props

--- app01/src/componentes/Corpo.js
+++ app01/src/componentes/Corpo.js
@@ -3,16 +3,41 @@ import Dados from './Dados'
 
 export default function Corpo(){
 
-    const cnl ='Tv Toyko'
+    /*const cnl ='Tv Toyko'
     const ep = '04'
-    const name = 'Naruto Shipudden'
+    const name = 'Naruto Shipudden'*/
 
+     /*function teste(){
+        return 'Ola mundo'
+    }*/
+
+    const cnl=()=>{
+        return 'Tv Toyko'
+    }
+
+    const ep=()=>{
+        return '04'
+    }
+
+    const name=()=>{
+        return 'Naruto'
+    }
+
+
+    const somar=(v1,v2)=>{
+        return v1+v2
+    }
 
     return(
         <>
         <section>
             <h2 align="center">Corpo</h2>
-            <Dados canal={cnl} episodio={ep} nome={name}/>
+            <Dados 
+            canal={cnl} 
+            episodio={ep} 
+            nome={name}
+            somar={somar}
+            />
         </section>
         
         </>
--- app01/src/componentes/Dados.js
+++ app01/src/componentes/Dados.js
@@ -4,14 +4,20 @@ import React from "react"
 
 export default function Dados(props){
 
+
+    let n1 = 30
+    let n2 = 115
+
+
 return (
 
     <>
     
     <section>
-        <p>Nome:{props.nome}</p>
-        <p>Episódios:{props.episodio}</p>
-        <p>Canal:{props.canal}</p>
+        <p>Nome:{props.nome()}</p>
+        <p>Episódios:{props.episodio()}</p>
+        <p>Canal:{props.canal()}</p>
+        <p>{'A soma de ' + n1 + ' com ' + n2 + ' é igual ' + props.somar(n1,n2)}</p>
     </section>
     
     </>
